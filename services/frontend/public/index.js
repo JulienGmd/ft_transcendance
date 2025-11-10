c332f8d8a3feb8592424aa5e854518be88ce7db5
JulienGmd <julien.grmd@gmail.com>
2025-11-10 04:26:46 +0100

    cleanup and remove my-link component to use an overload of <a> instead

--- services/frontend/public/index.js
+++ services/frontend/public/index.js
@@ -1,23 +1,20 @@
 // TODO typescript (nodemon or tsx should build this as .js so it can be served)
 
 const app = document.getElementById("app")
-/** name: content */
-const pages = {}
+const pages = {} /** pagePath: bodyContent */
 
 /**
  * Fetch or retrieve from cache the HTML content of a page
  * @param path The path of the page (e.g. /user/info)
- * @returns The HTML content of the page
+ * @returns The HTML content of the page or undefined if not found
  */
 async function getPage(path) {
   if (path === "/")
-    path = "home"
-  else
-    path = path.slice(1) // Remove leading '/'
+    path = "/home"
 
   if (!pages.hasOwnProperty(path)) {
     // Fetch the page content if not cached
-    const res = await fetch(`/public/${path}.html`)
+    const res = await fetch(`/public${path}.html`)
     if (res.ok)
       pages[path] = await res.text()
   }
@@ -25,7 +22,7 @@ async function getPage(path) {
   return pages[path]
 }
 
-/** Navigate to a different page, fetching content if necessary and updating the URL and history */
+/** Navigate to a different page, fetching content if necessary, updating the URL and history */
 async function navigate(to, pushHistory = true) {
   const page = await getPage(to)
   app.innerHTML = page || "<h1>404 Not Found</h1>"
@@ -33,26 +30,40 @@ async function navigate(to, pushHistory = true) {
     window.history.pushState({}, "", to)
 }
 
+/** @returns true if the <a> should be handled by the SPA navigation */
+function shouldHandleLink(a, e) {
+  return a.origin === location.origin // Same website
+    && a.target !== "_blank" // Not target blank (this is used to open link in new tab)
+    && e.button === 0 // Left click
+    && !e.ctrlKey && !e.metaKey && !e.shiftKey // No modifier
+}
+
+/** Handle browser navigation (back/forward buttons) */
 window.addEventListener("popstate", () => {
+  // Simply navigate to the url in the address bar
   navigate(window.location.pathname, false)
 })
 
-window.navigate = navigate // Expose navigate to global scope
-navigate(window.location.pathname) // Initial navigation
+document.addEventListener("mouseover", (e) => {
+  const a = e.target.closest("a")
+  if (!a || !shouldHandleLink(a, e))
+    return
 
-// ----- Components ----------------------------------------------------------------------------------------------------
+  // If hovering a handled <a>, preload the page
+  e.preventDefault()
+  getPage(a.pathname)
+})
 
-class MyLink extends HTMLElement {
-  connectedCallback() {
-    this.style.cursor = "pointer"
-    this.addEventListener("mouseover", (e) => {
-      e.preventDefault()
-      getPage(this.getAttribute("to"))
-    })
-    this.addEventListener("click", (e) => {
-      e.preventDefault()
-      navigate(this.getAttribute("to"))
-    })
-  }
-}
-customElements.define("my-link", MyLink)
+document.addEventListener("click", (e) => {
+  const a = e.target.closest("a")
+  if (!a || !shouldHandleLink(a, e))
+    return
+
+  // If clicking a handled <a>, navigate to the page
+  e.preventDefault()
+  navigate(a.pathname)
+})
+
+// On first navigation, /public/index.html and this script will be served,
+// if url is /user, this navigate will then display /public/user inside #app
+navigate(window.location.pathname)
