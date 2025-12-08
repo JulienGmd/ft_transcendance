abccf7474c7383888e94ecce183c99f9edee0c63
JulienGmd <julien.grmd@gmail.com>
2025-12-08 21:16:21 +0100

    remove unused files

--- .old/gateway/src/messaging/publisher.ts
+++
@@ -1,6 +0,0 @@
-import { getNatsClient } from "./index";
-
-export async function publish(topic: string, payload: object) {
-	const nc = getNatsClient();
-	nc.publish(topic, new TextEncoder().encode(JSON.stringify(payload)));
-}
--- types/types.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ApiEndpoint<Req, Res> {
-  req: Req
-  res: Res
-}
