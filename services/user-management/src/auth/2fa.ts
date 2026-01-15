18fc308a496688e3e88400f57b9aec68c4fb053c
jgrimaud <jgrimaud@student.42lyon.fr>
2026-01-15 17:30:14 +0100

    secure 2fa functions, rm debug

--- services/user-management/src/auth/2fa.ts
+++ services/user-management/src/auth/2fa.ts
@@ -2,26 +2,38 @@ import qrcode from "qrcode"
 import speakeasy from "speakeasy"
 
 export function generate2FASecret(email: string): { base32: string; otpauth_url: string } {
-  const secret = speakeasy.generateSecret({
-    name: `ft_transcendence: ${email}`,
-    length: 32,
-  })
+  try {
+    const secret = speakeasy.generateSecret({
+      name: `ft_transcendence: ${email}`,
+      length: 32,
+    })
 
-  return {
-    base32: secret.base32,
-    otpauth_url: secret.otpauth_url || "",
+    return {
+      base32: secret.base32,
+      otpauth_url: secret.otpauth_url || "",
+    }
+  } catch (error) {
+    return { base32: "", otpauth_url: "" }
   }
 }
 
 export async function generate2FAQrCode(otpauthUrl: string): Promise<string> {
-  return await qrcode.toDataURL(otpauthUrl)
+  try {
+    return await qrcode.toDataURL(otpauthUrl)
+  } catch (error) {
+    return ""
+  }
 }
 
 export function verify2FACode(secret: string, totp: string): boolean {
-  return speakeasy.totp.verify({
-    secret,
-    encoding: "base32",
-    token: totp,
-    window: 1,
-  })
+  try {
+    return speakeasy.totp.verify({
+      secret,
+      encoding: "base32",
+      token: totp,
+      window: 1,
+    })
+  } catch (error) {
+    return false
+  }
 }
